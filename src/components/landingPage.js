d7ac36dafea23d17e919a9f9ea52f701ad3eef4e
angel <angelsol299@hotmail.com>
2019-04-04 22:31:28 +0200

    refactoring components

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import LandingContent from "./landingContent";
+
 import Projects from "./projects/projects";
 import About from "./aboutMe";
 import FooterPage from "./footerPage";
@@ -8,23 +9,7 @@ class LandingPage extends Component {
   render() {
     return (
       <div style={{ width: "100%", margin: "auto" }}>
-        <Grid className="landing-grid">
-          <img
-            src="../../pics/power.jpg"
-            alt=""
-            style={{ width: "100%", height: "400px" }}
-          />
-          <Cell col={12}>
-            <div className="MiddleBanner">
-              <div className="banner-text">
-                <h1>Grupo SIME</h1>
-                <p style={{ textAlign: "center" }}>
-                  Tu energia en buenas manos
-                </p>
-              </div>
-            </div>
-          </Cell>
-        </Grid>
+        <LandingContent />
         <About id="about" />
         <hr />
         <Projects />
@@ -33,5 +18,4 @@ class LandingPage extends Component {
     );
   }
 }
-
 export default LandingPage;
