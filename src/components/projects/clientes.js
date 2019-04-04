713a88c6d66786a4d49b48dfc89deece7758caa6
angel <angelsol299@hotmail.com>
2019-04-04 22:47:58 +0200

    clients logos added

--- src/components/projects/clientes.js
+++ src/components/projects/clientes.js
@@ -42,7 +42,7 @@ class Clientes extends Component {
             }}
           />
         </Card>
-        {/*Project 1 */}
+        {/*Project 4 */}
         <Card shadow={0} style={{ minWidth: "450", margin: "auto" }}>
           <CardTitle
             style={{
@@ -51,11 +51,11 @@ class Clientes extends Component {
               width: "100%",
 
               background:
-                "url(http://www.plazavacationpromocion.com/wp-content/uploads/2017/04/Logo-plaza-pelicanos-01-1024x379.png) center / cover"
+                "url(http://www.papelpotosi.com.mx/Imagenes/Servilleta%20Publicitaria/Clientes/Hoteles/fiesta_americana_logo1.png) center / cover"
             }}
           />
         </Card>
-        {/*Project 1 */}
+        {/*Project 5 */}
         <Card shadow={0} style={{ minWidth: "450", margin: "auto" }}>
           <CardTitle
             style={{
@@ -64,11 +64,11 @@ class Clientes extends Component {
               width: "100%",
 
               background:
-                "url(http://www.plazavacationpromocion.com/wp-content/uploads/2017/04/Logo-plaza-pelicanos-01-1024x379.png) center / cover"
+                "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5_yrOZHEWe1QU32qAA3YCzYRMtMLrRDsXiUEmOu5CMt-0Mdq9) center / cover"
             }}
           />
         </Card>
-        {/*Project 1 */}
+        {/*Project 6 */}
         <Card shadow={0} style={{ minWidth: "450", margin: "auto" }}>
           <CardTitle
             style={{
@@ -77,7 +77,7 @@ class Clientes extends Component {
               width: "100%",
 
               background:
-                "url(http://www.plazavacationpromocion.com/wp-content/uploads/2017/04/Logo-plaza-pelicanos-01-1024x379.png) center / cover"
+                "url(https://manoamano.com/attachments/c38284650cde7a5e5722a4f181ea9a37aaf84633/store/fit/400/400/7779c5c86ae89e6d718b2a918d52c48430ec5001d368fa729b0fd9df7cfe/logo.gif) center / cover"
             }}
           />
         </Card>
