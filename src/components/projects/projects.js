3b3912e4b3c9cad5521d8396db0375d9c6cc5e93
angel <angelsol299@hotmail.com>
2019-04-06 00:24:18 +0200

    aspects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -27,6 +27,7 @@ class Projects extends Component {
   render() {
     return (
       <div className="category-tabs" style={{ marginTop: "0px" }}>
+        <hr />
         <Tabs
           style={{ backgroundColor: "white" }}
           className="tabs-bar"
