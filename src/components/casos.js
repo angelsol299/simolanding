8c612b9e2c87fe3ffa0003a4061af38fb2c7e683
angel <angelsol299@hotmail.com>
2019-04-08 20:12:56 +0200

    casos component

--- src/components/casos.js
+++ src/components/casos.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Casos = () => {
-  return <div>Casos</div>;
+  return (
+    <div style={{ width: "100%", height: "400px", backgroundColor: "#edf4ff" }}>
+      <h1>Casos</h1>
+      <p>
+        A continuación presentamos algunos de los muchos casos de éxito en
+        conjunto con nuestro curriculum con los que en conjunto con nuestros
+        clientes hemos logrado y a los cuales agradecemos su confianza a lo
+        largo de los años.
+      </p>
+      <button>Descargar</button>
+    </div>
+  );
 };
 
 export default Casos;
